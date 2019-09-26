fd00025a9b5043f8648ea0b576fa47e642cdfc38
Helloyyang <1281672109@qq.com>
2019-09-26 14:35:50 +0800

    将Fragment中的Me从Kotlin类型改为java类型

---
+++ app/src/main/java/com/example/r_clazz/Fragments/Me.java
@@ -0,0 +1,32 @@
+package com.example.r_clazz.Fragments;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.r_clazz.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Me extends Fragment {
+
+
+    public Me() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_me, container, false);
+    }
+
+}
